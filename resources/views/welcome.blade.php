97061557cef5f5d3cffad60c9ea0cdc6adc36873
falah aboud <falah@falahs-MacBook-Pro.local>
2018-01-22 00:28:32 +0100

    siehe das

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Falah habe gemacht Projekt also geht los mit laravel
+                    ich habe geschaft mein erste laravel projekt
                 </div>
 
                 <div class="links">
