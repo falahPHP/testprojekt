db1361dfd1b6e19111f1e07188e26a1be46f54c5
falah aboud <falah@falahs-MacBook-Pro.local>
2018-01-31 07:41:50 +0100

    Beschreibung
    ich habe beschreibung geändert

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    ich habe geschaft mein erste laravel projekt
+                    laravel projekt
                 </div>
 
                 <div class="links">
